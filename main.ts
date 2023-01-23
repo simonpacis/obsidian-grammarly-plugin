a7938831f5c114520575a275de4d7b8c8b225e89
Kyle Gabriel <kgabriel@email.sc.edu>
2023-01-22 22:04:46 -0500

    added the albility to log-in to grammarly account

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
-import { Extension } from '@codemirror/state';
-
-import { addIcon, App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import * as Grammarly from '@grammarly/editor-sdk'
-import { grammarlyPlugin } from './plugin'
+import { Extension } from "@codemirror/state";
+import { BrowserWindow } from "@electron/remote";
+import {
+	addIcon,
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	ObsidianProtocolHandler,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import * as Grammarly from "@grammarly/editor-sdk";
+import { grammarlyPlugin } from "./plugin";
 
 // Remember to rename these classes and interfaces!
 //
@@ -16,26 +27,36 @@ export interface ObsidianGrammarlyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: ObsidianGrammarlyPluginSettings = {
-	left_offset: '0',
-	top_offset: '0',
-	client_id: 'client_SZRuwBMe5opCznxqMQCG3q',
-	disable_tooltip: 'true'
-}
-
+	left_offset: "0",
+	top_offset: "0",
+	client_id: "DesktopClient_ID",
+	disable_tooltip: "true",
+};
 
 export default class ObsidianGrammarlyPlugin extends Plugin {
 	private ext: Extension;
 	private extArray: Extension[] = [];
 	settings: ObsidianGrammarlyPluginSettings;
 
-	async onload(){
-
+	async onload() {
 		await this.loadSettings();
 
-		addIcon('grammarly', '<svg fill="currentColor"  viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M24 12c0 6.627-5.373 12-12 12S0 18.627 0 12C0 5.372 5.373 0 12 0c6.628 0 12 5.372 12 12m-9.633 1.626a.81.815 0 0 0-.799.965c.071.393.44.662.84.662h1.257l.729-.102c-1.166 1.71-3.19 2.498-5.405 2.15-1.802-.282-3.35-1.502-4.003-3.205-1.483-3.865 1.34-7.556 5.02-7.556 1.916 0 3.598 1.122 4.562 2.478.277.39.763.504 1.133.248a.795.8 0 0 0 .236-1.069h.006a7.04 7.04 0 0 0-6.425-3.233c-3.508.236-6.347 3.107-6.55 6.617-.233 4.086 3.007 7.421 7.037 7.421a6.976 6.976 0 0 0 5.304-2.413l-.153.855v.773c0 .4.269.77.662.84a.814.814 0 0 0 .964-.8v-4.63h-4.415"/></svg>');
+		addIcon(
+			"grammarly",
+			'<svg fill="currentColor"  viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M24 12c0 6.627-5.373 12-12 12S0 18.627 0 12C0 5.372 5.373 0 12 0c6.628 0 12 5.372 12 12m-9.633 1.626a.81.815 0 0 0-.799.965c.071.393.44.662.84.662h1.257l.729-.102c-1.166 1.71-3.19 2.498-5.405 2.15-1.802-.282-3.35-1.502-4.003-3.205-1.483-3.865 1.34-7.556 5.02-7.556 1.916 0 3.598 1.122 4.562 2.478.277.39.763.504 1.133.248a.795.8 0 0 0 .236-1.069h.006a7.04 7.04 0 0 0-6.425-3.233c-3.508.236-6.347 3.107-6.55 6.617-.233 4.086 3.007 7.421 7.037 7.421a6.976 6.976 0 0 0 5.304-2.413l-.153.855v.773c0 .4.269.77.662.84a.814.814 0 0 0 .964-.8v-4.63h-4.415"/></svg>'
+		);
 
-		const ribbonIconEl = this.addRibbonIcon('grammarly', 'Enable Grammarly', (evt: MouseEvent) => {
-			this.enableGrammarly();
+		const ribbonIconEl = this.addRibbonIcon(
+			"grammarly",
+			"Enable Grammarly",
+			(evt: MouseEvent) => {
+				this.enableGrammarly();
+			}
+		);
+
+		this.registerObsidianProtocolHandler("grammarly-auth", (res) => {
+			const url = `obsidian://grammarly-auth?code=${res.code}&state=${res.state}`;
+			this.enableGrammarlyLogin(url);
 		});
 
 		this.addCommand({
@@ -52,15 +73,12 @@ export default class ObsidianGrammarlyPlugin extends Plugin {
 		}
 
 		this.addSettingTab(new MainSettingsTab(this.app, this));
-
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	enableGrammarly() {
-		new Notice('Grammarly has been enabled.');
+		new Notice("Grammarly has been enabled.");
 		const view = this?.app?.workspace?.activeLeaf?.view;
 		if (view != null) {
 			const editorView = (view as any).editor.cm;
@@ -73,37 +91,60 @@ export default class ObsidianGrammarlyPlugin extends Plugin {
 				/* Look for Grammarly-powered tooltip, and if found, remove element. */
 				const tooltip_observer = new MutationObserver(tooltip_callback);
 
-				function tooltip_callback (mutations: MutationRecord[]) {
-					for(var i = 0; i < mutations.length; i++)
-					{
+				function tooltip_callback(mutations: MutationRecord[]) {
+					for (var i = 0; i < mutations.length; i++) {
 						var mutation = mutations[i];
-						if(mutation.addedNodes.length > 0)
-							{
-								for(var ii = 0; ii < mutation.addedNodes.length; ii++)
-								{
-									var node = mutation.addedNodes[ii] as HTMLBodyElement;
-									if(node.innerText == "Grammarly-powered editor")
-										{
-											node.remove();
-										}
+						if (mutation.addedNodes.length > 0) {
+							for (
+								var ii = 0;
+								ii < mutation.addedNodes.length;
+								ii++
+							) {
+								var node = mutation.addedNodes[
+									ii
+								] as HTMLBodyElement;
+								if (
+									node.innerText == "Grammarly-powered editor"
+								) {
+									node.remove();
 								}
 							}
+						}
 					}
 				}
 
-				var tooltip_observer_element = document.querySelector("body")!;
+				const tooltip_observer_element =
+					document.querySelector("body")!;
 				console.log(this.settings);
 
-				if(this.settings.disable_tooltip == "true")
-					{
-					tooltip_observer.observe(tooltip_observer_element, {childList: true});
-					}
-
+				if (this.settings.disable_tooltip == "true") {
+					tooltip_observer.observe(tooltip_observer_element, {
+						childList: true,
+					});
+				}
 			}
 		}
 	}
+
+	enableGrammarlyLogin(url: string) {
+		const view = this?.app?.workspace?.activeLeaf?.view;
+		if (view != null) {
+			const editorView = (view as any).editor.cm;
+
+			const plugin = editorView.plugin(grammarlyPlugin);
+
+			if (plugin) {
+				plugin.authenticateUser(url, this.settings);
+			}
+		}
+	}
+
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -120,51 +161,65 @@ class MainSettingsTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Grammarly Plugin Settings'});
+		containerEl.createEl("h2", { text: "Grammarly Plugin Settings" });
 
 		new Setting(containerEl)
-		.setName('Left Popover Offset (in pixels)')
-		.setDesc('The Grammarly popover is placed incorrectly when using certain themes. If you encoutner this, this setting allows you to offset it to the left it so that it looks right.')
-		.addText(text => text
-						 .setPlaceholder('Enter offset (in pixels)')
-						 .setValue(this.plugin.settings.left_offset)
-						 .onChange(async (value) => {
-							 this.plugin.settings.left_offset = value;
-							 await this.plugin.saveSettings();
-						 }));
+			.setName("Left Popover Offset (in pixels)")
+			.setDesc(
+				"The Grammarly popover is placed incorrectly when using certain themes. If you encoutner this, this setting allows you to offset it to the left it so that it looks right."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter offset (in pixels)")
+					.setValue(this.plugin.settings.left_offset)
+					.onChange(async (value) => {
+						this.plugin.settings.left_offset = value;
+						await this.plugin.saveSettings();
+					})
+			);
 		new Setting(containerEl)
-		.setName('Top Popover Offset (in pixels)')
-		.setDesc('The Grammarly popover is placed incorrectly when using certain themes. If you encoutner this, this setting allows you to offset it from the top so that it looks right.')
-		.addText(text => text
-						 .setPlaceholder('Enter offset (in pixels)')
-						 .setValue(this.plugin.settings.top_offset)
-						 .onChange(async (value) => {
-							 this.plugin.settings.top_offset = value;
-							 await this.plugin.saveSettings();
-						 }));
+			.setName("Top Popover Offset (in pixels)")
+			.setDesc(
+				"The Grammarly popover is placed incorrectly when using certain themes. If you encoutner this, this setting allows you to offset it from the top so that it looks right."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter offset (in pixels)")
+					.setValue(this.plugin.settings.top_offset)
+					.onChange(async (value) => {
+						this.plugin.settings.top_offset = value;
+						await this.plugin.saveSettings();
+					})
+			);
 		new Setting(containerEl)
-		.setName('Grammarly Client ID')
-		.setDesc('The Grammarly Editor SDK requires a client ID to operate. One is included by default, but you can change it if you like.')
-		.addText(text => text
-						 .setPlaceholder('Client ID')
-						 .setValue(this.plugin.settings.client_id)
-						 .onChange(async (value) => {
-							 this.plugin.settings.client_id = value;
-							 await this.plugin.saveSettings();
-						 }));
+			.setName("Grammarly Client ID")
+			.setDesc(
+				"The Grammarly Editor SDK requires a client ID to operate. One is included by default, but you can change it if you like."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Client ID")
+					.setValue(this.plugin.settings.client_id)
+					.onChange(async (value) => {
+						this.plugin.settings.client_id = value;
+						await this.plugin.saveSettings();
+					})
+			);
 		new Setting(containerEl)
-		.setName('Remove Grammarly tooltip')
-		.setDesc('Set to false to show tooltip.')
-		.addText(text => text
-						 .setPlaceholder('Remove tooltip')
-						 .setValue(this.plugin.settings.disable_tooltip)
-						 .onChange(async (value) => {
-							 this.plugin.settings.disable_tooltip = value;
-							 await this.plugin.saveSettings();
-						 }));
+			.setName("Remove Grammarly tooltip")
+			.setDesc("Set to false to show tooltip.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Remove tooltip")
+					.setValue(this.plugin.settings.disable_tooltip)
+					.onChange(async (value) => {
+						this.plugin.settings.disable_tooltip = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
--- plugin.ts
+++ plugin.ts
@@ -4,84 +4,99 @@ import {
 	EditorView,
 	ViewPlugin,
 } from "@codemirror/view";
-import * as Grammarly from '@grammarly/editor-sdk';
-import {ObsidianGrammarlyPluginSettings} from './main';
+import * as Grammarly from "@grammarly/editor-sdk";
+import { ObsidianGrammarlyPluginSettings } from "./main";
 
-const initializeGrammarly = (view: EditorView, settings: ObsidianGrammarlyPluginSettings) => {
-const initialize = Grammarly.init(settings.client_id);
-	initialize.then((grammarly) => {
-		grammarly.addPlugin(
-			view.contentDOM,
-			{
-				documentDialect: 'auto-text', // 'british',
-				documentDomain: 'academic',
-				activation: 'immediate',
-			},
-			view.scrollDOM
-		);
+const initializeGrammarly = async (
+	view: EditorView,
+	settings: ObsidianGrammarlyPluginSettings
+) => {
+	const grammarly = await Grammarly.init(settings.client_id);
 
-		var host = document.querySelector("grammarly-editor-plugin");
-		var style = document.createElement('style');
-
-		// Not really sure how to style the shadow root in another way. This fixes positioning errors that for some reason occur with Grammarly SDK in Obsidian.
-
-
-		var inner_html  = `
-		.nvqxur1>:nth-child(2):not(article)
+	grammarly.addPlugin(
+		view.contentDOM,
 		{
-			left: 80px !important;
-		}
+			documentDialect: "auto-text", // 'british',
+			documentDomain: "academic",
+			activation: "immediate",
+		},
+		view.scrollDOM
+	);
 
-		div:has(div[aria-label="Grammarly Settings"])
-		{
-			left: 80px !important;
-		}
+	const host = document.querySelector("grammarly-editor-plugin");
+	const style = document.createElement("style");
 
-		div[role="tooltip"] {
-			left: 80px !important;
-		}
-		`;
+	// Not really sure how to style the shadow root in another way. This fixes positioning errors that for some reason occur with Grammarly SDK in Obsidian.
 
-		if(settings.left_offset != "0")
+	let inner_html = `
+	.nvqxur1>:nth-child(2):not(article)
+	{
+		left: 80px !important;
+	}
+
+	div:has(div[aria-label="Grammarly Settings"])
+	{
+		left: 80px !important;
+	}
+
+	div[role="tooltip"] {
+		left: 80px !important;
+	}
+	`;
+
+	if (settings.left_offset != "0") {
+		inner_html =
+			inner_html +
+			`
+			.nvqxur1 div[role=dialog]
 			{
-				inner_html = inner_html + `
-				.nvqxur1 div[role=dialog]
-				{
-					left: ` + settings.left_offset + `px !important;
-				}`
-			}
+				left: ` +
+			settings.left_offset +
+			`px !important;
+			}`;
+	}
 
-		if(settings.top_offset != "0")
+	if (settings.top_offset != "0") {
+		inner_html =
+			inner_html +
+			`
+			.nvqxur1 div[role=dialog]
 			{
-				inner_html = inner_html + `
-				.nvqxur1 div[role=dialog]
-				{
-					top: ` + settings.top_offset + `px !important;
-				}`
-			}
+				top: ` +
+			settings.top_offset +
+			`px !important;
+			}`;
+	}
 
-			style.innerHTML = inner_html;
+	style.innerHTML = inner_html;
 
-			host?.shadowRoot?.appendChild(style);
-	})
-}
+	host?.shadowRoot?.appendChild(style);
+
+	host?.setAttribute("config.oauthRedirectUri", "obsidian://grammarly-auth");
+};
+
+const authenticateGrammarlyUser = async (
+	url: string,
+	settings: ObsidianGrammarlyPluginSettings
+) => {
+	const grammarly = await Grammarly.init(settings.client_id);
+	grammarly.handleOAuthCallback(url);
+};
+
 class GrammarlyPlugin implements PluginValue {
-	constructor(view: EditorView) {
-	}
+	constructor(view: EditorView) {}
 
-	update(update: ViewUpdate) {
-	}
+	update(update: ViewUpdate) {}
 
-	destroy() {
-	}
+	destroy() {}
 
-	initialize(view: EditorView, settings: ObsidianGrammarlyPluginSettings){
+	initialize(view: EditorView, settings: ObsidianGrammarlyPluginSettings) {
 		return initializeGrammarly(view, settings);
 	}
 
-
+	authenticateUser(url: string, settings: ObsidianGrammarlyPluginSettings) {
+		return authenticateGrammarlyUser(url, settings);
+	}
 }
 
 export const grammarlyPlugin = ViewPlugin.fromClass(GrammarlyPlugin);
-
-
